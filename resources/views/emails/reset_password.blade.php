f04cfe43a1229abbf8230c88819bdf71a0dc07fb
Walshak Timothy Apollos <walshak1999@gmail.com>
2023-09-17 07:29:38 +0100

    modified reset pass mail view 1

--- resources/views/emails/reset_password.blade.php
+++ resources/views/emails/reset_password.blade.php
@@ -1,19 +1,18 @@
 @component('mail::message')
-# Introduction
 
-You are recieving this mail because yo have requested a password reset on your Adzmart account.
+    # Introduction
 
-# Reset Your Password
+    You are recieving this mail because yo have requested a password reset on your Adzmart account.
+    # Reset Your Password
 
-Click the button below to reset your password:
+    Click the button below to reset your password:
 
     @component('mail::button', ['url' => $resetUrl])
         Reset Password
     @endcomponent
 
-If you didn't request a password reset, no further action is required.
+    If you didn't request a password reset, no further action is required.
 
-
-Thanks,<br>
-{{ config('app.name') }}
+    Thanks,
+    {{ config('app.name') }}
 @endcomponent
